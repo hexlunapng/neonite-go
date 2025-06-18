9997cbfa3f3725b1a6807d691b42d428f95e88b5
Public-mrc <publicooo96@gmail.com>
2025-06-18 10:51:08 +0200

    Keychain download

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module neonite-go
 go 1.24.3
 
 require (
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/go-chi/chi/v5 v5.2.1 
+	github.com/google/uuid v1.6.0 
+	github.com/gorilla/mux v1.8.1 
 )
--- routes/shop.go
+++ routes/shop.go
@@ -1,15 +1,32 @@
 package routes
 
-
 import (
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
+
+func RegisterStorefrontRoutes(r *mux.Router) {
+
+	r.HandleFunc("/fortnite/api/storefront/v2/keychain", KeychainHandler).Methods("GET")
+}
 
 func CatalogHandler(w http.ResponseWriter, r *http.Request) {
 	file, err := os.ReadFile("shop.json")
 	if err != nil {
 		http.Error(w, `{"error":"Failed to read shop.json"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(file)
+}
+
+func KeychainHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.ReadFile("keychain.json") // keychain download :3
+	if err != nil {
+		http.Error(w, `{"error":"Failed to read keychain.json"}`, http.StatusInternalServerError)
 		return
 	}
 
